f813fbec9a1ba019dd9c1b815fb86f85d9e624c1
Kyle <kjfossman@gmail.com>
2021-03-17 05:48:38 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    
+    get '/' do 
+        "Hello World"
+        erb :user_input
+    end
+
+    post '/' do 
+        @words = PigLatinizer.new
+
+    end
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,32 @@
+require 'pry'
+class PigLatinizer
+    attr_accessor :words
+
+    def initialize
+        @words = words
+    end
+
+    def piglatinize_string(string)
+        a = string.split(" ")
+        b = a.map {|word| piglatinize(word)}
+        b.join(" ")
+    end
+
+    def piglatinize(word)
+        first_letter = word[0].downcase
+        vowels = ['a','e','i','o','u']
+        if vowels.include?(first_letter)
+            "#{word}way"
+        else
+            consonants = []
+            consonants << word[0]
+            if vowels.include?(word[1]) == false
+                consonants << word[1]
+                if vowels.include?(word[2]) == false
+                    consonants << word[2]
+                end
+            end
+            "#{word[consonants.length..-1] + consonants.join + "ay"}"
+        end
+    end
+end
